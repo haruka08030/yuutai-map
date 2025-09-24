dad9b502f3d8bc7a9489226896aeb8fe5f4598d8
Haruka S <haruka08030@gmail.com>
2025-09-24 14:56:10 -0700

    delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() async {
+import 'screens/home_screen.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const MyApp());
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  runApp(const MyApp()); // ← Supabase 初期化済みなので安全
 }
 
 class MyApp extends StatelessWidget {
