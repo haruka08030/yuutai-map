8f40922586476c93d1df55e63a583219fad2bdcf
Haruka S <haruka08030@gmail.com>
2025-10-17 20:22:13 -0700

    UI update

--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData get light => ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2A7DE1)),
-        useMaterial3: true,
-        listTileTheme: const ListTileThemeData(dense: false),
-      );
+  static ThemeData get light {
+    return ThemeData(
+      useMaterial3: true,
+      colorScheme: const ColorScheme.light(
+        primary: Color(0xFF2A7DE1),
+        surface: Colors.white,
+      ),
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        surfaceTintColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black87),
+        titleTextStyle: TextStyle(
+          color: Colors.black87,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      listTileTheme: const ListTileThemeData(dense: false),
+    );
+  }
 }
